a077098da2be7fd9689b73ed2ed01f365642fe26
julipahl <48629011+julipahl@users.noreply.github.com>
2019-07-03 12:13:39 +0200

    Add files via upload

---
+++ KhanyeziTokens.sol
@@ -0,0 +1,199 @@
+pragma solidity ^0.5;
+
+import "zeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+
+contract KhanyeziTokens is IERC20, Ownable {
+    // We inherit from two contracts: ERC20 to make it represent a fungible token, that can be traded
+    // and Ownable to manage authorization and restrict certain methods to the owner only, like minting
+    // Safe math is better for mathematical functions
+    using SafeMath for uint256;
+
+    /*
+    constructor (string name, string symbol, uint decimal, string memory totalSupply, string memory owner) public {
+        require(_price > 0, "Price of the token need to be positive");
+        require(ownerAddress != address(0) && ownerAddress != address(this), "Wrong address");
+
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+    }
+    */
+    // Don't need the above, will be done in the specific coin contract
+
+     // initially we transfer all the tokens to the contract owner
+
+    constructor () public {
+        // Initially assign all tokens to the contract's creator.
+        balanceOf[msg.sender].balance = _totalSupply;
+        owner = msg.sender;
+        emit Transfer(address(0), msg.sender, _totalSupply);
+    }
+
+    // Total number of tokens in existence
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+   }
+
+  // returns the account balance of another account with address _sender
+    function balanceOf(address _owner) external view returns (uint256){
+       return _balances[_owner];
+   }
+
+   // Function to check the amount of tokens that an owner("sender") allowed to a recipient.
+    function allowance(address _owner, address _to) external view returns (uint256){
+      return _allowed[_owner][_to];
+   }
+
+    // function to be used when investors buy the debt products form spv
+   function transfer(address _to, uint256 _amount) external returns (bool){
+        require(_amount <= _balances[msg.sender], "not enough money");
+        require(_to != address(0), "cant have that address");
+
+        owner = _to; // becomes new owner
+        emit Transfer(msg.sender, _to, _amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(_amount);
+        _balances[_to] = _balances[_to].add(_amount);
+        return true;
+   }
+
+    // function to use when repaying investor
+    function transferFrom(address _sender, address _to, uint256 _amount) external returns (bool){
+        require(_amount <= _balances[_sender], "value must be less than balance");
+        require(_amount <= _allowed[_sender][msg.sender], "value must be less than what is allowed to be sent");
+        require(_sender != address(0), "cant have that address");
+
+        _balances[_sender] = _balances[_sender].sub(_amount);
+        _balances[_to] = _balances[_to].add(_amount);
+
+        _allowed[_sender][msg.sender] = _allowed[_sender][msg.sender].sub(_amount); // subtract what is allowed to be spent
+        emit Transfer(_sender, _recipient, _amount);
+        return true;
+   }
+
+    // function to check that the sender (investor) approves the transfer of the tokens
+    function approve(address _sender, uint256 _amount) external returns (bool){
+        _allowed[msg.sender][_sender] = _amount;
+        emit Approval(msg.sender, _sender, _amount);
+        return true;
+        }
+
+    // function to be able to mint new coins
+    function _mint(address _account, uint256 _amount) internal {
+        require(_account != 0, "Account is not allowed to be account(0)");
+
+        _totalSupply = _totalSupply.add(_amount);
+        _balances[_account] = _balances[_account].add(_amount);
+        emit Transfer(address(0), _account, _amount);
+  }
+
+    function name() public view returns (string memory){
+        return _name;
+    }
+	function symbol() public view returns (string memory){
+        return _symbol;
+    }
+	function decimals() public view returns (uint8){
+        return _decimals;
+    }
+
+}
+
+contract KhanyeziSenior is KhanyeziTokens {
+
+// First create the senior tranche token
+
+    string internal constant name = "KhanyeziSenior";
+    string internal constant symbol = "K_SEN";
+    uint internal interest = 0.05;
+    uint internal decimals = 18; // how divisable you want your coins to be
+    address public ownerAddress; // current address of token holder
+    uint public price;
+    uint private totalSupply = 0.75*1000000;
+
+    constructor(string memory name, string memory symbol, uint memory interest, uint memory decimals, uint memory price) public {
+        // the contract is constructed once
+        require(_price > 0, "Price of the token need to be positive");
+        require(ownerAddress != address(0) && ownerAddress != address(this), "Wrong address");
+
+        price = _price;
+        name = _name;
+        symbol = _symbol;
+        interest = _interest;
+        decimals = _decimals;
+    }
+
+    constructor(string memory _totalSupply, string memory owner) public{
+        owner = msg.sender;
+        totalSupply = _totalSupply;
+    }
+}
+
+
+
+contract KhanyeziMezzanine is KhanyeziTokens {
+
+// Middle tranche
+    string internal constant name = "KhanyeziMezzanine";
+    string internal constant symbol = "K_MEZ";
+    uint internal interest = 0.06;
+    uint internal decimals = 18; // how divisable you want your coins to be
+    address public ownerAddress; // current address of token holder
+    uint public price;
+    uint private totalSupply = 0.15*1000000;
+
+    constructor(string memory name, string memory symbol, uint memory interest, uint memory decimals, uint memory price) public {
+        // the contract is constructed once
+        require(_price > 0, "Price of the token need to be positive");
+        require(ownerAddress != address(0) && ownerAddress != address(this), "Wrong address");
+
+        price = _price;
+        name = _name;
+        symbol = _symbol;
+        interest = _interest;
+        decimals = _decimals;
+    }
+
+    constructor(string memory _totalSupply, string memory owner) public{
+        owner = msg.sender;
+        totalSupply = _totalSupply;
+    }
+
+}
+
+
+contract KhanyeziEquity is KhanyeziTokens {
+
+// Equity tranche
+    string internal constant name = "KhanyeziEquity";
+    string internal constant symbol = "K_EQT";
+    uint internal interest = 0;
+    uint internal decimals = 18; // how divisable you want your coins to be
+    address public ownerAddress; // current address of token holder
+    uint public price;
+    uint private totalSupply = 0.1*1000000;
+
+    constructor(string memory name, string memory symbol, uint memory interest, uint memory decimals, uint memory price) public {
+        // the contract is constructed once
+        require(_price > 0, "Price of the token need to be positive");
+        require(ownerAddress != address(0) && ownerAddress != address(this), "Wrong address");
+
+        price = _price;
+        name = _name;
+        symbol = _symbol;
+        interest = _interest;
+        decimals = _decimals;
+    }
+
+    constructor(string memory _totalSupply, string memory owner) public{
+        owner = msg.sender;
+        totalSupply = _totalSupply;
+    }
+
+
+}
+
+
