8782edfae00a6ce0c0d901bd25c3e36c54bc3376
Julika Pahl <julipahl@gmail.com>
2019-07-22 16:28:41 +0200

    reformatted repo

--- contracts/InvestmentVehicle.sol
+++ contracts/InvestmentVehicle.sol
@@ -8,39 +8,42 @@ contract InvestmentVehicle {
 
     KhanyeziTokens khanyeziTokens;
 
+
     // call the owner of the contract SPV
     address payable private _SPV; // will send funds to the wallet
-
-    constructor(address _token) public {
+    uint256 public _totalAmount; // set a total amount needed every year we deploy a new contract
+    
+    constructor(address _token, uint256 _tokensNeeded) public {
        khanyeziTokens = KhanyeziTokens(_token);
+       _totalAmount = _tokensNeeded;
     }
 
     uint public TotalInvestors;
-
+    
     struct Investor {
         address InvestorAddrs;
         uint256 registerDate;
     }
-
+    
     Investor[] public investors;
-
+    
     // owner address to investor number "index"
     mapping (address => uint256) public _AddrsToInvestorNo;
 
-
+   
     event InvestorTransaction (
-        address _OwnerAddrs,
-        uint _amount,
-        uint _depositeDate
+        address OwnerAddrs,
+        uint amount,
+        uint depositeDate
     );
-
+    
 
     // register investment, by updating the Investment struct from the KhanyeziTokens contract
     function registerInvestor() public returns(uint) {
         // get an instance of Investor using the input variables and push into the array of songs, returns the id
 
         uint index = investors.push(Investor(msg.sender, now)) - 1;
-
+        
         _AddrsToInvestorNo[msg.sender] = index; // to get the index for an address
         // return the investor id
         return index;
@@ -48,27 +51,58 @@ contract InvestmentVehicle {
 
     /* Returns the total number of holders of this currency. */
     function TokenHolderCount() public view returns (uint256) {
-        investors.length;
+        return investors.length;
     }
-
+    
+    function totalTokensNeeded() public view returns (uint256) {
+        return _totalAmount;
+    }
+    
+    function totalSupply() public view returns (uint256) {
+        return khanyeziTokens.totalSupply();
+    }
+    
     // public so that investors can call the function and buy tokens
+    // as the investor purchases tokens, tokens are minted
     function buyTokens(uint256 _amount) public payable {
         require(msg.value >= _amount, "not enough funds sent");
-        require(khanyeziTokens.totalSupply() >= _amount, "There must be enough tokens on sale");
+        require(khanyeziTokens.totalSupply() + _amount <= _totalAmount, "There must be enough tokens on sale");
 
         // call the transfer function to transfer from tokens_contract specfic token to investor (msg.sender)
-        khanyeziTokens.transfer(msg.sender, _amount);
-
+        khanyeziTokens.mint(msg.sender, _amount);
+    
         _SPV.transfer(msg.value);
 
         //send ether to SPV wallet
-
+    
         uint256 _depositeDate = now;
         emit InvestorTransaction(msg.sender, _amount, _depositeDate);
 
     }
 
-        // check investment 
+    // check investment 
+
+
+    function InvestmentValue() public view returns (uint256) {
+        // first need to see whether this is a registered address
+        // require(_AddrsToInvestorNo[msg.sender] != 0, "Not an investor");
+
+        return khanyeziTokens.InvestorAmount(msg.sender);
+    }
+
+    function InvestorDepositeDate() public view returns (uint256) {
+        return khanyeziTokens.InvestorDate(msg.sender);
+    }
+
+    function interest() public view returns (uint256) {
+        uint256 _interest = khanyeziTokens.interest();
+        return _interest;
+    }
+
+
+}
+
+ // check investment 
 
     /*
 
@@ -91,4 +125,3 @@ contract InvestmentVehicle {
 */
 
 
-}
